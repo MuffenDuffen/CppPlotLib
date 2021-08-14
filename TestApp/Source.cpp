3b61a37cada317fb5ef6a611683e84d7887cf49d
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-08-14 19:02:15 +0200

    initial commit

---
+++ TestApp/Source.cpp
@@ -0,0 +1,112 @@
+#include "CppPlotLib.h"
+#include <vector>
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow window;
+	auto windowColor = sf::Color(0, 0, 0, 255);
+	window.create(sf::VideoMode(800, 600), "Test Window");
+	window.setFramerateLimit(60);
+
+	#pragma region Font loading
+	sf::Font font;
+	if (!font.loadFromFile("JetBrainsMono-Medium.ttf"))
+	{
+		std::cout << "Failed to load font file\n";
+		exit(EXIT_FAILURE);
+	}
+	#pragma endregion 
+	
+	sf::Text text0;
+	text0.setString("LOL");
+	text0.setFillColor(sf::Color(0, 255, 0));
+	text0.setFont(font);
+	text0.setCharacterSize(48);
+
+	std::vector texts { text0 };
+
+	while (window.isOpen())
+	{
+		#pragma region Events
+
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+			{
+				sf::FloatRect visibleArea(0, 0, (event.size.width), (event.size.height));
+				window.setView(sf::View(visibleArea));
+					break;
+			}
+			case sf::Event::LostFocus: 
+				break;
+			case sf::Event::GainedFocus: 
+				break;
+			case sf::Event::TextEntered: 
+				break;
+			case sf::Event::KeyPressed:
+				break;
+			case sf::Event::KeyReleased:
+				break;
+			case sf::Event::MouseWheelMoved: 
+				break;
+			case sf::Event::MouseWheelScrolled: 
+				break;
+			case sf::Event::MouseButtonPressed:
+				break;
+			case sf::Event::MouseButtonReleased:
+				break;
+			case sf::Event::MouseMoved: 
+				break;
+			case sf::Event::MouseEntered: 
+				break;
+			case sf::Event::MouseLeft: 
+				break;
+			case sf::Event::JoystickButtonPressed: 
+				break;
+			case sf::Event::JoystickButtonReleased: 
+				break;
+			case sf::Event::JoystickMoved: 
+				break;
+			case sf::Event::JoystickConnected: 
+				break;
+			case sf::Event::JoystickDisconnected: 
+				break;
+			case sf::Event::TouchBegan: 
+				break;
+			case sf::Event::TouchMoved: 
+				break;
+			case sf::Event::TouchEnded: 
+				break;
+			case sf::Event::SensorChanged: 
+				break;
+			case sf::Event::Count: 
+				break;
+			}
+		}
+
+		#pragma endregion
+		
+		#pragma region Changes
+
+		#pragma endregion
+		
+		#pragma region Rendering
+
+		window.clear(windowColor);
+
+		for (const auto& text : texts) window.draw(text);
+		
+		window.display();
+		
+		#pragma endregion 
+	}
+	
+	return 0;
+}
