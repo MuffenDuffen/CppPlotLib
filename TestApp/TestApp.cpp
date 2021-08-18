a5988b488e9695efb31407db89334ce0d9886856
XpulseIV <malteaxner363@gmail.com>
2021-08-18 21:26:33 +0200

    what is this?

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -5,5 +5,9 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    CppP::showDiagram();
+    std::vector<unsigned long long> nums = { 1,2,3,4,5,4,3,2,1 };
+
+    const CppP::DataStruct dataRemZ(nums, "Test Window", "XAxis", "YAxis");
+
+    CppP::showDiagram(dataRemZ);
 }
